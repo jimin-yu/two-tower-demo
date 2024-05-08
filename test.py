00fbb26e65e0549c898d6833c3807f18b1656273
유지민 <86466319+jimin-yu@users.noreply.github.com>
2024-05-08 15:36:11 +0900

    fix: vector db로 weaviate 대신 milvus를 사용하도록 변경

--- test.py
+++ test.py
@@ -1,21 +1,4 @@
-import requests
-import json
-import weaviate
-client = weaviate.Client("https://weaviate.dev.sinsang.market")
-# client.is_ready()
-url = 'https://raw.githubusercontent.com/weaviate-tutorials/quickstart/main/data/jeopardy_tiny+vectors.json'
-resp = requests.get(url)
-data = json.loads(resp.text)
-# Configure a batch process
-client.batch.configure(batch_size=100)  # Configure batch
-with client.batch as batch:
-    # Batch import all Questions
-    for i, d in enumerate(data):
-        print(f"importing question: {i+1}")
-        properties = {
-            "answer": d["Answer"],
-            "question": d["Question"],
-            "category": d["Category"],
-        }
-        print(properties)
-        # batch.add_data_object(properties, "Question", vector=d["Vector"])
+from pymilvus import MilvusClient
+client = MilvusClient(uri='http://milvus.dev.sinsang.market:19530')
+
+client.create_collection(collection_name='test_collection', dim)
