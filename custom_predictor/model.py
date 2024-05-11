716cc55afe5cf9eb5f7e9df1fbea7902018a7d41
유지민 <86466319+jimin-yu@users.noreply.github.com>
2024-05-12 02:25:35 +0900

    feat: custom predictor

---
+++ custom_predictor/model.py
@@ -0,0 +1,28 @@
+import kserve
+import joblib
+import pandas as pd
+from typing import Dict
+
+class CatBoostModel(kserve.Model):
+    def __init__(self, name: str):
+       super().__init__(name)
+       self.name = name
+       self.load()
+
+    def load(self):
+        self.model = joblib.load('model.pkl')
+
+    def predict(self, payload: Dict) -> Dict:
+        input_data = payload["inputs"][0]
+        ranking_features = input_data["ranking_features"]
+        articles_id = input_data["articles_id"]
+        
+        input_df = pd.DataFrame(ranking_features)
+        scores = self.model.predict_proba(input_df)[:,1]
+        predictions = { "scores": scores, "articles_id": articles_id }
+
+        return {"predictions": predictions}
+
+if __name__ == "__main__":
+    model = CatBoostModel("catboost-model")
+    kserve.ModelServer().start([model])
