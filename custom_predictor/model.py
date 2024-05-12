967831502c618ef3db67b049cfb2be4b5c2d4049
유지민 <86466319+jimin-yu@users.noreply.github.com>
2024-05-12 20:04:08 +0900

    fix: custom-catboost-model로 이름 변경

--- custom_predictor/model.py
+++ custom_predictor/model.py
@@ -24,5 +24,5 @@ class CatBoostModel(kserve.Model):
         return {"predictions": predictions}
 
 if __name__ == "__main__":
-    model = CatBoostModel("ranking-model")
+    model = CatBoostModel("custom-catboost-model")
     kserve.ModelServer().start([model])
